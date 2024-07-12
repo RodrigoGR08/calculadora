1f3d14d060728a476ffd2adf6eb31d19f8cf5cd5
Rodrigo Garcia Ramalho <rodrigogarcia.ramalho@gmail.com>
2024-07-11 20:42:20 -0400

    Adicionando funcoes aos botões
    Introduzinho funções com retornos simples em javascript, a ser aperfeiçoado

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function AdicionarNumero(x){ 
+    let resultado = document.getElementById("resultado");   
+    resultado.innerHTML += x
+}
+function Somar(){
+    console.log("somar")
+}
+function Subtrair(){
+    console.log("subtrair")
+}
+function Multiplicar(){
+    console.log("multiplicar")
+}
+function Dividir(){
+    console.log("dividir")
+}
+function AtualizarResultado(){
+    console.log("atualizar resultado")
+}
